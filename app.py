855c33e8888f3e0efc25512649b7110dcb86b77f
sahintelli <36115381+sahintelli@users.noreply.github.com>
2024-08-16 17:22:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def oku(dosya):
             icerik += sayfa.extract_text()
         return icerik
     elif dosya_yolu.endswith(".docx"):
-        icerik = docx2txt.process(dosya_yolu)
+        icerik = docx2txt.process(dosya)
         # doc = Document(dosya_yolu)
         # parca = [para.text for para in doc.paragraphs]
         # icerik = '\n'.join(parca)
