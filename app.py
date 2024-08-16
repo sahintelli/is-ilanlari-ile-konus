95251f4c3ebea0beae9e2db7a45ba7b620ea0206
sahintelli <36115381+sahintelli@users.noreply.github.com>
2024-08-16 18:26:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -137,24 +137,18 @@ def main():
         st.session_state.mesajlar = [
         {"role": "system", "content": "Verilen is ilanlari ile ilgili yardimci bir asistansin."}
     ]
-    
+
     icerik = ''
     for dosya in uploaded_files:
       icerik += f"Dosya ismi {dosya.name} icerisindeki icerik basladi: "
       icerik += oku(dosya)
       icerik += f"Dosya ismi {dosya.name} icerisindeki icerik bitti. "
     # st.write(f"Dosya okundu. Icerik: {icerik}")
-    
-    if not "dosya_icerigi" in st.session_state:
-        st.session_state.dosya_icerigi = []
-    elif st.session_state.dosya_icerigi[-1] == icerik:
-        st.write('bos yada ayni')
-        pass
-    else:
-        if not icerik:
-            st.session_state.dosya_icerigi.append(icerik)
-            st.session_state.mesajlar.append({"role": "user", "content": f"Dosya icerigi: {icerik}"})
     st.write(st.session_state)
+
+    if not icerik:
+        st.session_state.mesajlar.append({"role": "user", "content": f"Dosya icerigi: {icerik}"})
+        
 
     
     # If API key and uploaded files are provided, display the file names and types
